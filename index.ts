99493d313f957627f455089ff0a59dd38f8b61f0
s <info@elitepictures.co>
2026-03-07 16:15:06 -0800

    Add price alert notifications with OneSignal

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ import os from "os";
 import cors from "cors";
 import fs from "fs";
 import path from "path";
+import { startPriceAlertService } from "./priceAlertService";
+import usersRouter from "./usersRoute";
 
 import pkg from "pg";
 const { Pool } = pkg;
@@ -531,6 +533,8 @@ app.use(cors({
 }));
 app.use((_, res, next) => { res.setHeader("Vary", "Origin"); next(); });
 app.use((req, res, next) => { if (req.method === "OPTIONS") return res.sendStatus(204); next(); });
+app.use("/users", usersRouter); 
+
 
 /* --------------------------- diagnostics -------------------------- */
 
